c50ff096b06f64821d7c62b8dace16585e17e7ec
Michael Pruitt <michael@sharethetaste.org>
2023-01-21 23:39:39 -0600

    Add files via upload
    initial commit

---
+++ app/code/Lw/AzureDemo/Controller/Index/Index.php
@@ -0,0 +1,11 @@
+<?php
+namespace Lw\AzureDemo\Controller\Index;
+ 
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        echo 'Hello, World!';
+    }
+}
+?>
---
+++ app/code/Lw/AzureDemo/registration.php
@@ -0,0 +1,7 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Lw_AzureDemo',
+    __DIR__
+);
+?>
